64b480d6313055377686842895ea059298e9d913
brianfluk <brianf.luk@gmail.com>
2018-09-26 16:15:10 -0400

    routing, survey

--- food/src/app/app.module.ts
+++ food/src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SurveyComponent } from './survey/survey.component';
-import { SearchComponent } from './search/search.component';
+import { SurveyComponent } from './components/survey/survey.component';
+import { SearchComponent } from './components/search/search.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
